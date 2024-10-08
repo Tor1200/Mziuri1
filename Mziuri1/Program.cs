8b1090c3290b4f95bbc73399957977c010caf810
Tor1200 <129094695+Tor1200@users.noreply.github.com>
2024-10-08 18:20:48 +0400

    Inital Commit

---
+++ Mziuri1/Program.cs
@@ -0,0 +1,29 @@
+﻿//ამოცანა 1.1
+
+int a11 = Convert.ToInt32(Console.ReadLine()), b11 = Convert.ToInt32(Console.ReadLine()), c11;
+c11 = a11;
+a11 = b11;
+b11 = c11;
+Console.WriteLine($"{a11} " + $"{b11}");
+
+
+//ამოცანა 1.2
+int a12 = Convert.ToInt32(Console.ReadLine()), b12 = Convert.ToInt32(Console.ReadLine());
+b12 -= a12;
+a12 += b12;
+b12 = a12 - b12;
+Console.WriteLine($"{a12} " + $"{b12}");
+
+//ამოცანა 2
+double d2 = Convert.ToDouble(Console.ReadLine());
+int n2 = Convert.ToInt32(d2);
+double pas2 = d2 - n2;
+Console.WriteLine(pas2);
+
+//ამოცანა 3
+//int n3 = Convert.ToInt32(Console.ReadLine());
+
+//ამოცანა 4
+
+//ბონუსი
+bool a4 = Convert.ToBoolean(Console.ReadLine()), b4 = Convert.ToBoolean(Console.ReadLine());
